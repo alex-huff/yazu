6481dbabc4fdcecb12fcd6de766fcf413a9d77e6
alex-huff <alexhuff@bakahuff.com>
2026-04-07 17:29:04 -0500

    wayland-scanner boilerplate

--- include/yazu.h
+++ include/yazu.h
@@ -2,6 +2,16 @@
 #define _YAZU_H
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <wayland-client.h>
+
+#include "wlr-layer-shell-unstable-v1-client-protocol.h"
+
+struct yazu {
+	struct wl_compositor *wl_compositor;
+	struct wl_shm *wl_shm;
+	struct zwlr_layer_shell_v1 *layer_shell;
+};
 
 #endif
--- main.c
+++ main.c
@@ -2,8 +2,16 @@
 
 // BEGIN REGISTRY
 
-static void registry_handle_global(void *data, struct wl_registry *registry,
+static void registry_handle_global(void *data, struct wl_registry *wl_registry,
 		uint32_t name, const char *interface, uint32_t version) {
+	struct yazu *state = data;
+	if (strcmp(interface, wl_compositor_interface.name) == 0) {
+		state->wl_compositor = wl_registry_bind(wl_registry, name, &wl_compositor_interface, 6);
+	} else if (strcmp(interface, wl_shm_interface.name) == 0) {
+		state->wl_shm = wl_registry_bind(wl_registry, name, &wl_shm_interface, 2);
+	} else if (strcmp(interface, zwlr_layer_shell_v1_interface.name) == 0) {
+		state->layer_shell = wl_registry_bind(wl_registry, name, &zwlr_layer_shell_v1_interface, 5);
+	}
 }
 
 static void registry_handle_global_remove(void *data, struct wl_registry
@@ -20,12 +28,29 @@ static const struct wl_registry_listener registry_listener = {
 int main(int argc, char **argv) {
 	struct wl_display *display = wl_display_connect(NULL);
 	if (!display) {
-		fprintf(stderr, "Failed to connect to Wayland display\n");
-		return 1;
+		fprintf(stderr, "failed to connect to Wayland display\n");
+		return EXIT_FAILURE;
 	}
+	struct yazu state = {
+	};
 	struct wl_registry *registry = wl_display_get_registry(display);
-	wl_registry_add_listener(registry, &registry_listener, NULL);
+	wl_registry_add_listener(registry, &registry_listener, &state);
+
+	// roundtrip for registry
 	wl_display_roundtrip(display);
+
+	if (state.wl_compositor == NULL) {
+		fprintf(stderr, "compositor doesn't support wl_compositor\n");
+		return EXIT_FAILURE;
+	}
+	if (state.wl_shm == NULL) {
+		fprintf(stderr, "compositor doesn't support wl_shm\n");
+		return EXIT_FAILURE;
+	}
+	if (state.layer_shell == NULL) {
+		fprintf(stderr, "compositor doesn't support zwlr_layer_shell_v1\n");
+		return EXIT_FAILURE;
+	}
 	wl_display_disconnect(display);
 	return 0;
 }
