8cc9390314869173adbe8af1aa3dc09052a544e5
alex-huff <alexhuff@bakahuff.com>
2026-04-07 16:09:53 -0500

    Wayland registry boilerplate

---
+++ include/yazu.h
@@ -0,0 +1,7 @@
+#ifndef _YAZU_H
+#define _YAZU_H
+
+#include <stdio.h>
+#include <wayland-client.h>
+
+#endif
--- main.c
+++ main.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
-#include <wayland-client.h>
+#include "yazu.h"
+
+// BEGIN REGISTRY
+
+static void registry_handle_global(void *data, struct wl_registry *registry,
+		uint32_t name, const char *interface, uint32_t version) {
+}
+
+static void registry_handle_global_remove(void *data, struct wl_registry
+		*registry, uint32_t name) {
+}
+
+static const struct wl_registry_listener registry_listener = {
+	.global = registry_handle_global,
+	.global_remove = registry_handle_global_remove,
+};
+
+// END REGISTRY
 
 int main(int argc, char **argv) {
 	struct wl_display *display = wl_display_connect(NULL);
@@ -7,6 +23,9 @@ int main(int argc, char **argv) {
 		fprintf(stderr, "Failed to connect to Wayland display\n");
 		return 1;
 	}
+	struct wl_registry *registry = wl_display_get_registry(display);
+	wl_registry_add_listener(registry, &registry_listener, NULL);
+	wl_display_roundtrip(display);
 	wl_display_disconnect(display);
 	return 0;
 }
